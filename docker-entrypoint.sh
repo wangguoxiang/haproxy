17e72252c60f0048ccc6fdb99cd44bb4e08deafc
Your Name <you@example.com>
2018-02-28 16:15:32 +0800

    update rsyslog

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,24 @@
 #!/bin/sh
 set -e
 
+mkdir /var/log/haproxy
+chmod a+w /var/log/haproxy
+
+echo '# Provides UDP syslog reception \
+      $ModLoad imudp \
+      $UDPServerRun 514 \
+      # Save haproxy log \
+	  local0.*                       /var/log/haproxy/haproxy.log' >>/etc/rsyslog.conf
+
+mkdir /etc/sysconfig
+echo '# Options for rsyslogd \
+# Syslogd options are deprecated since rsyslog v3. \
+# If you want to use them, switch to compatibility mode 2 by "-c 2" \
+# See rsyslogd(8) for more details \
+SYSLOGD_OPTIONS="-r -m 0 -c 2" '>/etc/sysconfig/rsyslog
+
+service rsyslog restart
+
 # first arg is `-f` or `--some-option`
 if [ "${1#-}" != "$1" ]; then
 	set -- haproxy "$@"
